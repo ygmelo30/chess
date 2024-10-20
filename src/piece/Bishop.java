468de4603d52da977b3042e1e0ebb0f319fff6bd
Yasser G <yghulais@gmail.com>
2024-10-20 03:03:56 -0400

    fixed queen and bishop they cant move through pieces now

--- src/piece/Bishop.java
+++ src/piece/Bishop.java
@@ -29,72 +29,38 @@ public class Bishop  extends Piece{
     @Override
     public boolean isPieceInPath(int targetCol, int targetRow) {
         if(targetRow > prevRow && targetCol > prevCol) {
-            int itr = Math.abs(targetRow - prevRow);
-            for(int i = 0; i < itr; i++) {
-                System.out.println("i is " +  i);
+            for(int i = 0; i < Math.abs(targetRow - prevRow); i++) {
                 for(Piece piece : GamePanel.simPieces) {
-                    System.out.println("row is " + prevRow + i);
-                    System.out.println("col is " + prevRow + i);
-                    System.out.println("end of this iteration");
-                    System.out.println("i is " +  i);
-                    System.out.println("first" );
                     if(piece.row == prevRow + i && piece.col == prevCol + i && piece != this) {
                         return true;
                     }
                 }
-                System.out.println("end of checking this sqaure");
             }
         } else if(targetRow > prevRow && targetCol < prevCol) {
-            int itr = Math.abs(targetRow - prevRow);
-            for(int i = 0; i < itr; i++) {
-                System.out.println("i is " +  i);
+            for(int i = 0; i < Math.abs(targetRow - prevRow); i++) {
                 for(Piece piece : GamePanel.simPieces) {
-                    System.out.println("row is " + prevRow + i);
-                    System.out.println("col is " + (targetCol - i));
-                    System.out.println("end of this iteration");
-                    System.out.println("i is " +  i);
-                    System.out.println("second");
-                    if(piece.row == prevRow + i && piece.col == targetCol - i && piece != this) {
+                    if(piece.row == prevRow + i && piece.col == prevCol - i && piece != this) {
                         return true;
                     }
                 }
-                System.out.println("end of checking this sqaure");
             }
         } else if(targetRow < prevRow && targetCol < prevCol) {
-            int itr = Math.abs(targetRow - prevRow);
-            for(int i = 0; i < itr; i++) {
-                System.out.println("i is " +  i);
+            for(int i = 0; i < Math.abs(targetRow - prevRow); i++) {
                 for(Piece piece : GamePanel.simPieces) {
-                    System.out.println("i is " +  i);
-                    System.out.println("row is " + (targetRow - i));
-                    System.out.println("col is " + (targetCol- i));
-                    System.out.println("prevrow is " + prevRow);
-                    System.out.println("prevcol is " + prevCol);
-                    System.out.println("end of this iteration");
-                    System.out.println("third");
-                    if(piece.row == (prevRow - i) && piece.col == (prevCol - i) && piece != this) {
+                    if(piece.row == prevRow - i && piece.col == prevCol - i && piece != this) {
                         return true;
                     }
                 }
             }
         } else if(targetRow < prevRow && targetCol > prevCol) {
-            int itr = Math.abs(targetRow - prevRow);
-            for(int i = 0; i < itr; i++) {
-                System.out.println("i is " +  i);
+            for(int i = 0; i < Math.abs(targetRow - prevRow); i++) {
                 for(Piece piece : GamePanel.simPieces) {
-                    System.out.println("i is " +  i);
-                    System.out.println("row is " + (prevRow - i));
-                    System.out.println("col is " + (targetCol+ i));
-                    System.out.println("end of this iteration");
-                    System.out.println("fourth");
-                    if(piece.row == prevRow + i && piece.col == targetCol - i && piece != this) {
+                    if(piece.row == prevRow - i && piece.col == prevCol + i && piece != this) {
                         return true;
                     }
                 }
-                System.out.println("end of checking this sqaure");
             }
         }
-
         return false;
         }
     }
--- src/piece/Queen.java
+++ src/piece/Queen.java
@@ -21,12 +21,86 @@ public class Queen  extends Piece{
         if(isWithinBoard(targetCol, targetRow)) {
 
             if((prevCol == targetCol || prevRow == targetRow)) {
-                if(isValidSquare(targetCol, targetRow)) {
+                if(isValidSquare(targetCol, targetRow) && !isPieceInPath(targetCol, targetRow)) {
                     return true;
                 }
             } else if ((Math.abs(targetCol - prevCol)) == (Math.abs(targetRow - prevRow))) {
-                if(isValidSquare(targetCol, targetRow)) {
+                if(isValidSquare(targetCol, targetRow) && !isPieceInPath(targetCol, targetRow)) {
                     return true;
+                }
+            }
+        }
+        return false;
+    }
+    @Override
+    public boolean isPieceInPath(int targetCol, int targetRow) {
+        if((prevCol == targetCol) || (prevRow == targetRow)) {
+            if(prevRow > targetRow) {
+                for (int i = prevRow; i > targetRow; i--) {
+                    for(Piece piece : GamePanel.simPieces) {
+                        if(piece.row == i && piece.col == targetCol && piece != this) {
+                            return true;
+                        }
+                    }
+                }
+            } else if(targetRow > prevRow) {
+                for (int i = prevRow; i < targetRow; i++) {
+                    for(Piece piece : GamePanel.simPieces) {
+                        if(piece.row == i && piece.col == targetCol && piece != this) {
+                            return true;
+                        }
+                    }
+                }
+            } else if(targetCol > prevCol) {
+                for (int i = prevCol; i < targetCol; i++) {
+                    for(Piece piece : GamePanel.simPieces) {
+                        if(piece.row == targetCol && piece.col == i && piece != this) {
+                            return true;
+                        }
+                    }
+                }
+            } else if(prevCol > targetCol) {
+                for (int i = prevCol; i > targetCol; i--) {
+                    for(Piece piece : GamePanel.simPieces) {
+                        if(piece.row == targetRow && piece.col == i && piece != this) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            if (targetRow > prevRow && targetCol > prevCol) {
+                for (int i = 0; i < Math.abs(targetRow - prevRow); i++) {
+                    for (Piece piece : GamePanel.simPieces) {
+                        if (piece.row == prevRow + i && piece.col == prevCol + i && piece != this) {
+                            return true;
+                        }
+                    }
+                }
+            } else if (targetRow > prevRow && targetCol < prevCol) {
+                for (int i = 0; i < Math.abs(targetRow - prevRow); i++) {
+                    for (Piece piece : GamePanel.simPieces) {
+                        if (piece.row == prevRow + i && piece.col == prevCol - i && piece != this) {
+                            return true;
+                        }
+                    }
+                }
+            } else if (targetRow < prevRow && targetCol < prevCol) {
+                for (int i = 0; i < Math.abs(targetRow - prevRow); i++) {
+                    for (Piece piece : GamePanel.simPieces) {
+                        if (piece.row == prevRow - i && piece.col == prevCol - i && piece != this) {
+                            return true;
+                        }
+                    }
+                }
+            } else if (targetRow < prevRow && targetCol > prevCol) {
+                for (int i = 0; i < Math.abs(targetRow - prevRow); i++) {
+                    for (Piece piece : GamePanel.simPieces) {
+                        if (piece.row == prevRow - i && piece.col == prevCol + i && piece != this) {
+                            return true;
+                        }
+                    }
                 }
             }
         }
