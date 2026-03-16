e8c55aa92998347d95531a20bf44012be42daa9d
Yasser G <yghulais@gmail.com>
2026-03-16 02:39:24 -0400

    added shell scripts

---
+++ run.bat
@@ -0,0 +1,2 @@
+javac -d bin src\*.java src\main\*.java src\piece\*.java
+java -cp "bin;resources" Main
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-package main;
+import main.GamePanel;
 
 import javax.sound.sampled.*;
 import javax.swing.*;
